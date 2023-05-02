26fd54bd2217fc490155d53970e2e18961ff576a
Corey Baughman <corey.j.baughman@gmail.com>
2023-05-02 09:24:56 -0500

    updataed project files, added explore.ipynb and location_predictor_final_report.ipynb

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 014_personal_project
-Repo for CodeUp Data Science Personal Project
+**Repo for CodeUp Data Science Personal Project**
 
 Project Goal:
 
@@ -17,6 +17,10 @@ Plan of Attack:
     - Census tract number should be the 'primary key' of this dataset: i.e. each row is a census tract and each column is a feature of that tract.
     - Use distance based modeling to predict where there are studios and compare to where there are actually studios.
     - What sort of model would work best for this? Regression would be able to predict? This seems like a perfect application for KMeans Clustering. So need to make all of the features continuous.
+
+### Background
+
+Census data had to be acquired from the U.S. Census Bureau. Below are instructions to retrieve that data.
 
 Census Links:
 
@@ -75,3 +79,85 @@ env.py file:
 
 ``` api = 'Replace with Google Places API key' '''
 
+## Acquire
+
+* Three basic types of data were required:
+    1. Census question data
+    1. Census tract shapefile data
+    1. Business search results data
+* Select Census question data was acquired from U.S. Census Bureau website.
+* Data was from the 2020 census.
+* Data was downloaded as three .csv files and imported into pandas dataframes which were combined on census tract number a.k.a. 'geography'
+* The data was limited to Bexar County, TX which has 375 census tracts. Each of these represents a row.
+* The census tract shapefiles were required to assign point from search data into census tracts.
+* The shapefiles were also retrieved from the U.S. Census Bureau website.
+* Business search results data was retrieved using the Google Places API.
+* Geopandas was then used to assign the locations retrieved to census tracts.
+
+## Prepare
+
+#### Data Cleaning Steps:
+- Used following to clean data:
+    1. Renamed columns to readable, pythonic names for each DataFrame.
+    2. Standardized Geography names across DataFrames
+    3. Dropped Columns from Census question DataFrames to select an initial feature set. (Entire tables must be downloaded as .csv and many features are heavily subsetted)
+    4. Combined three census question DataFrames on 'geography' which was standardized and had 375 rows for every DataFrame.
+    5. There were four federal tracts that had total populations of zero and those were dropped.
+    6. Search results from Google Places were checked against census tracts to verify they were in Bexar County else they were dropped. 
+    7. There were a total of 13 yoga studios in the county.
+    8. Chose to ignore outliers for simplicity in the MVP and since underlying distibutions were not heavily skewed.
+    9. There were no null values other than the four zero population tracts.
+
+## Exploration
+
+#### Insights from Univariate Exploration:
+
+    1. Features are relatively normally distributed.
+    2. Target classes are heavily imbalanced
+    1. The sample size for the target is very small
+    
+#### Insights from Bivariate Exploration:
+
+    1. There are slight positive correlations between having a yoga studio and income as well as being further north geograpically
+    2. There is a slight negative correlation between having a yoga studio and the child dependency ratio.
+    
+
+#### Clustering Summary:
+
+- visually, there are no distinct clusters at k=3 to k=7
+- will add clusters with k=5 as a feature for modeling anyway.
+
+
+### Exploration Summary: What Drives Studio Placement?
+1. higher household median income
+2. placement further north in the county
+3. lower child dependency ratio
+4. Initial clustering exploration did not produce strong clusters visually
+
+## Modeling
+
+#### **Goal:** The goal of my modeling phase is to build a model that predicts whether a census tract will have a yoga studio.
+    
+#### **Evaluation**: Models will be evaluated by the recall metric:
+    1. against a baseline model by accuracy
+        - accuracy measures total number of correct predictions over the total number of observations. It is a useful measure against the baseline model
+    2. against each other using recall
+        - recall measures the number of True Positive Predictions divided by total number of actual positive observations. I'm using it because I want to cast a wide net for areas that may be good candidates for a studio. It can't be checked against baseline, but is the best measure for these models against each other.
+        
+#### The baseline model is created by replacing all predicted values with the most common class from the actual values. In this case that is 'False' for no yoga studio in the census tract.
+
+### Modeling Wrap 
+* With an accuracy of .96 and a recall of 0.00 for the True class, the current Random Forest Classifier model fails to beat the baseline model of guessing 'no studio' for any given census tract.
+
+## Conclusion
+
+### Summary
+* Some drivers of business placement were found
+* The current classification model fails to beat baseline 
+
+
+### Next Steps
+1. Add features that measure distance between studios and tract centroids
+2. Explore other algorithms for modeling
+3. Test with a larger dataset
+4. Work on balancing classes
--- acquire.py
+++ acquire.py
@@ -1,7 +1,28 @@
 # acquire.py
+# ============================================================
+
+
+# IMPORTS:
+
+# basic imports
 import numpy as np
 import pandas as pd
-from location import get_tract_centroids
+import os
+
+# location/GIS imports
+import geopandas as gpd
+from shapely.geometry import Point
+
+# imports to interact with google places API
+import requests
+import json
+
+# local modules
+from env import api # import API key
+# ============================================================
+
+
+# CENSUS DATA ACQUISITION:
 
 def get_sex_age_data():
     '''
@@ -98,6 +119,159 @@ def get_census_data():
     '''
     df = pd.merge(pd.merge(pd.merge(get_sex_age_data(), get_race_data(), on='geography'), get_income_data(), on='geography')
                  , get_tract_centroids(), on='geography')
+    df = df[df.total_pop != 0]
+    df = df.astype(
+        {'sex_ratio':'float','old_age_dep_ratio':'float', 
+         'child_dep_ratio':'float','household_med_income':'int', 
+         'centroid_lat':'float', 'centroid_long':'float'})
+    df = df.reset_index(drop=True)
     
     return df
 
+# ============================================================
+
+
+#  GOOGLE PLACES DATA ACQUISITION:
+
+
+def get_tract_centroids():
+    '''
+    Takes in the 2020 US Census Shapefile for Bexar County,
+    TX and returns a df with geography number, centroid_latitude,
+    and centroid longitude for each geography.
+    See README for instructions to acquire shapefile.
+    
+    Arguments: None.
+    
+    Returns: DataFrame with columns 'geography', 'centroid_lat', 
+    and 'centroid_long'.
+    '''
+    # Load census tract shapefile
+    tracts = gpd.read_file('tl_rd22_48_tract')
+    # select geography, centroid lat, centroid long
+    df = tracts[(tracts.COUNTYFP == '029')]
+    df = df[['TRACTCE', 'INTPTLAT', 'INTPTLON']]
+    df = df.rename(columns={'TRACTCE':'geography', 
+                     'INTPTLAT':'centroid_lat', 'INTPTLON':'centroid_long'})
+    return df
+    
+
+def assign_census_tract(df):
+    '''
+    This function loads a census tract shapefile
+    to a DataFrame using geopandas. It then takes
+    in a DataFrame of latitude and longitude values and
+    adds a column indicating what 2020 U.S. Census tract 
+    each location is in or None if the location is not
+    in Bexar County. Returns the DataFrame.
+    See README for instructions to acquire shapefile.
+    
+    Arguments: DataFrame with columns 'latitude' and
+    'longitude'.
+    
+    Returns: DataFrame with columns 'latitude', 'longitude',
+    and 'census_tract'.
+    '''
+    # Load census tract shapefile
+    tracts = gpd.read_file('tl_rd22_48_tract')
+    # filter tracts to those in Bexar County
+    tracts = tracts[(tracts.COUNTYFP == '029')]
+    # convert df into a list of tuples
+    coords = df.values.tolist()
+    # add census tract column to df
+    df['census_tract'] = None
+    # Create point object
+    for index, coord in enumerate(coords):
+        point = Point(coord[1], coord[0])
+        # Check if point is within a census tract
+        tract = tracts[tracts.contains(point)]
+        # Extract data from corresponding row in dataframe
+        if not tract.empty:
+            tract_data = tract.iloc[0]
+            df.loc[index, 'census_tract'] = tract_data['NAME']
+        else:
+            df.drop(index=index, inplace=True)
+            
+    return df
+
+
+
+def get_bexar_yoga_studios(api=api):
+    '''
+    This function searches google places API for yoga
+    studios within 50km radius of the Bexar County 
+    Courthouse. That includes all of Bexar County and
+    may capture some area outside the county. Then filtration
+    against census tract shapefiles is performed to remove
+    results from outside the county. A google places API key
+    is required for function use.
+    
+    Arguments: google places API key
+    
+    Returns: DataFrame of yoga studios in Bexar County, TX, USA.
+    '''
+    # set up API endpoint and parameters
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    params = {
+        'location': '29.4241,-98.4936',  # Bexar County Courthouse, TX coordinates
+        'radius': '50000',  # search radius in meters
+        'keyword': 'yoga studio',  # keyword to search for
+        'key': api  # replace with your own API key
+    }
+
+    # send HTTP request to API and get response
+    response = requests.get(url, params=params)
+    data = json.loads(response.text)
+
+    # extract latitude and longitude coordinates for each result
+    coordinates = []
+    for result in data['results']:
+        lat = result['geometry']['location']['lat']
+        lng = result['geometry']['location']['lng']
+        coordinates.append((lat, lng))
+        
+    # convert coordinates to a pandas DataFrame
+    df = pd.DataFrame(coordinates, columns=['latitude', 'longitude'])
+    
+    # lookup census tract for each location
+    df = assign_census_tract(df)
+    
+    # remove locations not in Bexar County and standardize
+    # geography names
+    df = clean_yoga_df(df)
+    
+    return df
+
+
+def clean_yoga_df(df):
+    '''
+    Function takes in the output of get_bexar_yoga_studios()
+    and removes any locations that are not in a Bexar County
+    US Census tract. It also converts the census tract names
+    to match the geography names in the census data.
+    '''
+    df = df[df.census_tract != None]
+    df['geography'] = (
+        (df.census_tract.astype('float')) * 100).astype('int').astype('str')
+    df = df.drop(columns='census_tract')
+    
+    return df
+
+
+def get_combined_data():
+    '''
+    Funtion takes in the dataframes from the census and
+    also the one from Google Places and outputs a single
+    DataFrame.
+    '''
+    df = pd.merge(get_census_data(), get_bexar_yoga_studios(), 
+                  on = 'geography', how= 'left')
+    df['has_yoga'] = df.latitude >= 0
+    
+    return df
+
+
+
+
+
+# ============================================================
